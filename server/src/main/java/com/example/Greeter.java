ac7e092efe609251b07f971e5c306368cb6005a7
suvarnarajukaladi <114375276+suvarnarajukaladi@users.noreply.github.com>
2022-11-04 17:47:40 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello, %s!", Welcome to World of Devops);
+    return String.format("Hello, %s!", someone);
   }
 }
